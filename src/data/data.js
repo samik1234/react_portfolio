982e32ccd3c82f5bfcc1bf0ba3e7808823ebb745
samiksha <samikshawankhade11@gmail.com>
2023-04-12 19:49:58 +0530

    initial commit

--- src/data/data.js
+++ src/data/data.js
@@ -1,35 +1,53 @@
-import WorkIm from '../Assets/projects/workImg.jpeg'
-import RealEst from '../Assets/projects/realestate.jpg'
+import chat from '../Assets/projects/chat.jpeg';
+import installNode from '../Assets/projects/installNode.jpg'
+import arrayDestruct from '../Assets/projects/arrayDestruct.png'
+import reactSmooth from '../Assets/projects/reactSmooth.jpeg';
+import  useState  from '../Assets/projects/useState.jpg';
+import bumble from '../Assets/projects/bumble.jpeg';
+
 
 export const data=[
     {
         id:1,
         name:"React JS Application",
-        image:WorkIm,
-        github:"",
-        live:"",
+        image:chat,
+        github:"https://github.com/samik1234/ChatAi",
+        live:"https://chat-ai-blond.vercel.app/",
     },
     {
         id:2,
         name:"React JS Application",
-        image:RealEst,
-        github:"",
-        live:"",
+        image:bumble,
+        github:"https://github.com/samik1234/bumble-next-app",
+        live:"https://bumble-next-app.vercel.app/",
     },
     {
         id:3,
         name:"React JS Application",
-        image:WorkIm,
-        github:"",
-        live:"",
+        image:installNode,
+        github:"https://github.com/samik1234/my-foodiewe",
+        live:"https://samikshawankhademy-foodiewe.netlify.app/",
     },
     {
         id:4,
         name:"React JS Application",
-        image:RealEst,
-        github:"",
-        live:"",
+        image:arrayDestruct,
+        github:"https://github.com/samik1234/MY-WEEKAWAY",
+        live:"https://sammedia-weatherapi.netlify.app/",
     },
-
+    {
+        id:5,
+        name:"React JS Application",
+        image:reactSmooth,
+        github:"https://github.com/samik1234/dalle-2.0",
+        live:"https://sam-dalle-clone-2.netlify.app/",
+    },
+    {
+        id:6,
+        name:"React JS Application",
+        image:useState,
+        github:"https://github.com/samik1234/yt-clone-react",
+        live:"https://sammedia-yt-clone.netlify.app/",
+    },
 
 ]
